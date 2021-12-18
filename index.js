80cc3510a6347cb023209e533d35e8a0bbf99d85
Acel1 <axel.caron@gmail.com>
2021-12-18 17:17:08 +0100

    animated context, bounces while hitting side

--- index.js
+++ index.js
@@ -3,14 +3,28 @@ const context = canvas.getContext("2d");
 
 let btn = document.getElementById("btn-play");
 
-canvas.width = 600;
+canvas.width = 800;
 canvas.height = 600;
 
-let x = Math.random() * 550;
-let y = Math.random() * 550;
-function object() {
-  return new context.fillRect(x, y, 50, 50);
-}
 btn.addEventListener("click", () => {
-  object();
+  let x = Math.random() * 750;
+  let y = Math.random() * 550;
+  let vx = 1;
+  let vy = 1;
+  function anim() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    requestAnimationFrame(anim);
+    context.fillRect(x, y, 50, 50);
+
+    if (x + 50 > canvas.width || x < 0) {
+      vx = -vx;
+    }
+    if (y + 50 > canvas.height || y < 0) {
+      vy = -vy;
+    }
+    x += vx;
+    y += vy;
+  }
+  anim();
 });
